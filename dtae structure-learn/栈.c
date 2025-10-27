99d83ac1e7925c209986824cfb8a7d2801c7e390
cixia-zhao <2135952859@qq.com>
2025-10-27 23:28:15 +0800

    栈定义

---
+++ dtae structure-learn/栈.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAXSIZE 10
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int ElemType;
+typedef struct
+{
+
+	ElemType date[MAXSIZE];
+	int top;
+}Sqstack;
+
+int main()
+{
+
+
+
+	return 0;
+}
